c583229330550106709d712e1459de1ca03bfdc3
Ben Hodes <bhodes@myseneca.ca>
2026-05-15 14:36:38 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Ben Hodes
+#Author ID: 131538241
+#Date Created: 2026/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
