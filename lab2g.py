24ad1e91e1413ef98e6292989d5181c63003c710
Ben Hodes <bhodes@myseneca.ca>
2026-05-15 14:37:02 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Author: Ben Hodes
+#Author ID: 131538241
+#Date Created: 2026/05/15
+
+import sys
+
+if len(sys.argv) != 2:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
